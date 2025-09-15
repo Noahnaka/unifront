396f9cd8adf82b69d3e8a4b89674ddf5e49ad4e4
Noahnaka <Noahnakashima@gmail.com>
2025-09-15 15:50:10 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Fights from "./pages/Fights";
-import Betting from "./pages/Betting";
 import Leaderboard from "./pages/Leaderboard";
 import NotFound from "./pages/NotFound";
 import Aposta from "./pages/Aposta";
@@ -31,7 +30,6 @@ const AppContent = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/fights" element={<Fights />} />
-        <Route path="/betting" element={<Betting />} />
         <Route path="/leaderboard" element={<Leaderboard />} />
         <Route path="/aposta" element={<Aposta />} />
         <Route path="/premium" element={<Premium />} />
